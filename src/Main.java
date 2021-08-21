bb485465cb78f9a48cf87a477e4f3510eb31c800
Nguyễn Huỳnh Đức <nguyenhuynhduc96vp@gmail.com>
2021-08-21 15:15:25 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree<String> tree = new BinarySearchTree<>();
+        tree.insert("George");
+        tree.insert("Michael");
+        tree.insert("Tom");
+        tree.insert("Adam");
+        tree.insert("Jones");
+        tree.insert("Peter");
+        tree.insert("Daniel");
+        tree.insert("Duc");
+        tree.insert("Phuong");
+        tree.insert("Jerry");
+
+        System.out.println("Inorder (sorted): ");
+        tree.inorder();
+        System.out.println("The number of nodes is: " + tree.getSize());
+    }
+}
